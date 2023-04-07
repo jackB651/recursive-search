6c23ca2012ab5c379b89d8b0659411fcebddd47f
Jonathon Bialas <105708083+jackB651@users.noreply.github.com>
2023-04-06 23:13:09 -0500

    Add files via upload

---
+++ ruby/recursive_search.rb
@@ -0,0 +1,21 @@
+def recursive_search(arr, target)
+  # type your code in here
+    return false if arr.empty?
+    return true if arr.first == target
+    recursive_search(arr[1..-1], target);
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Expecting: true"
+  puts "=>", recursive_search([1, 2, 3], 2)
+
+  puts
+
+  puts "Expecting: false"
+  puts "=>", recursive_search([3, 2, 1], 4)
+
+  # Don't forget to add your own!
+end
+
+# Please add your pseudocode to this file
+# And a written explanation of your solution
